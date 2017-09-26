77df09c4d9cd27ad3a2e8060e3a372c0d0675031
Young Jung <ryviusm6@gmail.com>
2017-09-26 23:24:33 +0900

    Add Venues list in sidebar

--- neighborhood_map/neighborhood_map.py
+++ neighborhood_map/neighborhood_map.py
@@ -27,3 +27,81 @@ def show_neighborhood_map():
     """Serve Neighborhood Map application """
     return render_template('neighborhood_map.html',
                            GOOGLE_MAP_API_KEY=GOOGLE_MAP_API_KEY)
+
+
+@app.route('/venues')
+def get_venues_list():
+    """Fetch Venues' data near requested address from Foursquare API
+    And return JSON object for the neighborhood map app.
+    """
+
+    # Get parameters from request
+    address = request.args.get('address')
+    section = request.args.get('section')
+
+    # If one of parameters is missing, send error response
+    if address is None:
+        response = create_response('PARAM_ERROR', 'Must provide param address', 400)  # noqa
+        return response
+    if section is None:
+        response = create_response('PARAM_ERROR', 'Must provide param section', 400)  # noqa
+        return response
+
+    # Use Google Maps to convert address into Latitude/Longitude coordinates
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    params = {
+        'address': address,
+        'key': GOOGLE_MAP_API_KEY
+    }
+    answer = requests.get(url, params=params)
+    data = answer.json()
+    # If there's no result from Google Maps API, send error response
+    if data.get('status') == 'ZERO_RESULTS':
+        response = create_response('WRONG_ADDRESS', 'No such address', 200)
+        return response
+    # If error occurs while getting lat/lng, send error response
+    if data.get('status') != 'OK':
+        response = create_response('G_MAPS_ERROR', "Couldn't get lat&lng", 200)
+        return response
+
+    # Get Latitude/Longitude from Google Maps API response
+    lat = data.get('results')[0].get('geometry').get('location').get('lat')
+    lng = data.get('results')[0].get('geometry').get('location').get('lng')
+    # Use Foursquare API to get venues data using lat/lng and section
+    url = 'https://api.foursquare.com/v2/venues/explore'
+    params = {
+        'client_id': FOURSQUARE_CLIENT_ID,
+        'client_secret': FOURSQUARE_CLIENT_SECRET,
+        'v': 20170901,
+        'll': str(lat) + ',' + str(lng),
+        'section': section,
+        'venuePhotos': 1
+    }
+    answer = requests.get(url, params=params)
+    data = answer.json()
+    # If error occurs while getting venues data, send error response
+    if data.get('meta').get('code') != 200:
+        response = create_response('FOURSQUARE_ERROR', 'Error occured while getting venues data from Foursquare', 200)  # noqa
+        return response
+
+    # Make response and send venues data to client
+    message = 'Venues List for {} in {}'.format(section, address)
+    results = data.get('response').get('groups')[0]
+    results['location'] = {'lat': lat, 'lng': lng}
+    response = create_response('OK', message, 200, results)
+    return response
+
+
+def create_response(status, message, code, results={}):
+    """Create customized response object
+    """
+    message = {
+        'meta': {
+            'status': status,
+            'message': message
+        },
+        'results': results
+    }
+    response = make_response(json.dumps(message), code)
+    response.headers['Content-Type'] = 'application/json'
+    return response
--- neighborhood_map/static/js/app.js
+++ neighborhood_map/static/js/app.js
@@ -19,6 +19,38 @@ function initMap() {
 
 
 /**
+*
+* Class to represent a list item in the venues list
+* @param {Object} venueItem A JSON-serializable object of venue data
+*/
+const Venue = function(venueItem) {
+    const self = this;
+
+    self.name = venueItem.venue.name;
+    self.address = venueItem.venue.location.formattedAddress.join(' ');
+    self.lat = venueItem.venue.location.lat;
+    self.lng = venueItem.venue.location.lng;
+    self.phone = venueItem.venue.contact.formattedPhone ? venueItem.venue.contact.formattedPhone : '';
+    self.url = venueItem.venue.url ? venueItem.venue.url : '';
+    if (venueItem.venue.featuredPhotos) {
+        self.photo = venueItem.venue.featuredPhotos.items[0].prefix + '300x300';
+        self.photo += venueItem.venue.featuredPhotos.items[0].suffix;
+    } else {
+        self.photo = "static/img/no_image.png";
+    }
+    self.formattedName = ko.computed(function(){
+        if (self.name.length > 25) {
+            return self.name.slice(0, 25) + '...';
+        } else {
+            return self.name;
+        }
+    });
+};
+
+
+
+
+/**
  *
  * ViewModel - Knockout.js ViewModel
  *
@@ -26,7 +58,62 @@ function initMap() {
 let ViewModel = function() {
     const self = this;
 
-    self.locations = locations;
+    self.city = ko.observable('Busan');
+    self.country = ko.observable('South Korea');
+    self.availableSections = ['TopPicks', 'Food', 'Drinks', 'Coffee', 'Shops', 'Arts', 'Outdoors', 'Sights', 'Trending', 'Specials'];
+    self.section = ko.observable(self.availableSections[0]);
+    self.filter = ko.observable('');
+    self.venues = ko.observableArray([]);
+    self.isWaiting = ko.observable(false);
+    self.isInfo = ko.observable(false);
+    self.infoTitle = ko.observable('');
+    self.infoMessage = ko.observable('');
+
+    self.searchVenues = function() {
+        let address = self.city() + ', ' + self.country();
+        let url = '/venues';
+        url += '?' + $.param({
+            'address': address,
+            'section': self.section()
+        });
+        self.venues([]);
+        self.isWaiting(true);
+        self.isInfo(false);
+        $.ajax({
+            url: url,
+            method: 'GET',
+        }).done(function(response) {
+            // console.log(response.meta);
+            if (response.meta.status === 'OK') {
+                // console.log(response.results.location);
+                map.setCenter(response.results.location);
+                const venueItems = response.results.items;
+                if (venueItems.length === 0) {
+                    self.displayInfo('No result', "Couldn't find " + self.section() + " in this location.");
+                } else {
+                    for (const venueItem of venueItems) {
+                        const venue = new Venue(venueItem);
+                        self.venues.push(venue);
+                    }
+                    self.isWaiting(false);
+                }
+            } else if (response.meta.status === 'WRONG_ADDRESS') {
+                self.displayInfo('Location Error', "Couldn't find the location. Please specify City and Country.");
+            } else {
+                self.displayInfo('Server Error', "Couldn't receive data from the server.");
+            }
+        }).fail(function(err) {
+            // console.log(err);
+            self.displayInfo('Server Error', "Couldn't receive data from the server.");
+        });
+    };
+
+    self.displayInfo = function(title, message) {
+        self.infoTitle(title);
+        self.infoMessage(message);
+        self.isWaiting(false);
+        self.isInfo(true);
+    };
 };
 
 let viewModel = new ViewModel();

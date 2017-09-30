21fda2412f3cb4106a126fe886901d65d9c3ca9a
Young Jung <ryviusm6@gmail.com>
2017-09-30 19:39:44 +0900

    Add comments

--- neighborhood_map/static/js/app.js
+++ neighborhood_map/static/js/app.js
@@ -1,3 +1,6 @@
+/**
+* Default Settings for initializing the app
+*/
 const DEFAULT_DATA = {
     city: 'Busan',
     country: 'South Korea',
@@ -8,8 +11,12 @@ const DEFAULT_DATA = {
     section: 'TopPicks'
 };
 
+
+/**
+* An object stores data and functions releted with Google Maps API
+*/
 let googleMap = {
-    // Initialize variables for Google Maps API
+    // Declare variables for Google Maps API
     map: null,
     center: null,
     infoWindow: null,
@@ -17,6 +24,12 @@ let googleMap = {
     // Styles are stored in './google_map_styles.js' file
     styles: google_map_styles,
 
+
+    /**
+    * Create a marker for a venue
+    * @param {Object} venue A Venue object
+    * @return {Object} google.maps.Marker
+    */
     createMarker: function(venue) {
         let marker = new google.maps.Marker({
             map: this.map,
@@ -30,16 +43,19 @@ let googleMap = {
         });
         this.bounds.extend(marker.position);
 
+        // On mouseover, the marker and list item in sidebar are focused
         marker.addListener('mouseover', function() {
             $('li[data-venue-id="' + venue.id + '"]').addClass('focused');
             viewModel.focusMarker(venue);
         });
 
+        // Add listener to remove focus
         marker.addListener('mouseout', function() {
             $('li[data-venue-id="' + venue.id + '"]').removeClass('focused');
             viewModel.unfocusMarker(venue);
         });
 
+        // When clicked, the venue in viewModel is chosen
         marker.addListener('click', function() {
             viewModel.chooseVenue(venue);
         });
@@ -47,14 +63,23 @@ let googleMap = {
         return marker;
     },
 
+
+    /**
+    * Populates the infoWindow on that markers position when the marker is clicked.
+    * @param {Object} marker google.maps.Marker object
+    * @param {Object} venue A Venue object
+    * @param {Object} infoWindow google.maps.InfoWindow object
+    */
     populateInfoWindow: function(marker, venue, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
         if (infowindow.marker != marker) {
+            // Create content of the infowindow
             let content = '<div class="iw__container">';
             content += '<div class="iw__title">' + venue.name + '</div>';
             content += '<div class="iw__content">';
             content += '<div class="iw__img__container"><img class="iw__img" src="' + venue.photo + '" alt="' + venue.name + 'picture"></div>';
             content += '<p>' + venue.address + '</p>';
+            // Phone and URL are only displayed when it's defined
             if (venue.phone) {
                 content += '<p><bold>Phone : </bold>' + venue.phone + '</p>';
             }
@@ -62,10 +87,13 @@ let googleMap = {
                 content += '<p><bold>URL : </bold><a href="' + venue.url +'">' + venue.url + '</p>';
             }
             content += '</div></div>';
+
+            // Display infowindow
             infowindow.marker = marker;
             infowindow.setContent(content);
             infowindow.open(googleMap.map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
+
+            // When the infowindow is closed, remove focus on marker
             infowindow.addListener('closeclick', function() {
                 marker.setIcon('/static/img/icon_default.png');
                 infowindow.marker = null;
@@ -76,11 +104,8 @@ let googleMap = {
 };
 
 
-
-
 /**
- *
- * Class to represent a list item in the venues list
+ * Class to represent a venue in the venues list
  * @param {Object} venueItem A JSON-serializable object of venue data
  */
 const Venue = function(venueItem) {
@@ -93,12 +118,14 @@ const Venue = function(venueItem) {
     self.phone = venueItem.venue.contact.formattedPhone;
     self.url = venueItem.venue.url;
     self.id = venueItem.venue.id;
+    // When photo isn't available, set default image for the venue
     if (venueItem.venue.featuredPhotos) {
         self.photo = venueItem.venue.featuredPhotos.items[0].prefix + '300x300';
         self.photo += venueItem.venue.featuredPhotos.items[0].suffix;
     } else {
         self.photo = "static/img/no_image.jpg";
     }
+    // When venue name is to long to fit in sidebar, slice the name
     self.formattedName = ko.computed(function() {
         if (self.name.length > 25) {
             return self.name.slice(0, 25) + '...';
@@ -106,6 +133,7 @@ const Venue = function(venueItem) {
             return self.name;
         }
     });
+    // Marker is assigned after Venue is instantiated
     self.marker = null;
 };
 
@@ -113,51 +141,67 @@ const Venue = function(venueItem) {
 
 
 /**
- *
- * ViewModel - Knockout.js ViewModel
- *
- */
+* ViewModel - Knockout.js ViewModel
+* @param {Object} data Data to initialize ViewModel
+*/
 let ViewModel = function(data) {
     const self = this;
 
+    // Availabe section parameters for the Foursquare API
+    self.availableSections = ['TopPicks', 'Food', 'Drinks', 'Coffee', 'Shops', 'Arts', 'Outdoors', 'Sights', 'Trending', 'Specials'];
+    // Variables for the search venues form
     self.city = ko.observable(data.city);
     self.country = ko.observable(data.country);
-    self.availableSections = ['TopPicks', 'Food', 'Drinks', 'Coffee', 'Shops', 'Arts', 'Outdoors', 'Sights', 'Trending', 'Specials'];
     self.section = ko.observable(data.section);
-    self.filter = ko.observable('');
+    // Variables for the venue list
     self.venues = ko.observableArray([]);
     self.chosenVenue = ko.observable();
+    // Variable for the filter form
+    self.filter = ko.observable('');
+    // Flag variables to display filter, loading image, infoMessage
     self.isFiltered = ko.observable(false);
     self.isWaiting = ko.observable(false);
-    self.isInfo = ko.observable(false);
+    self.showInfo = ko.observable(false);
+    // Variables for displaying information to user
     self.infoTitle = ko.observable('');
     self.infoMessage = ko.observable('');
 
+
+    /**
+    * Get venues data from server, update the ViewModel,
+    * and store data in localStorage.
+    */
     self.searchVenues = function() {
+        // Prepare url and parameters to get data
         let address = self.city() + ', ' + self.country();
         let url = '/venues';
         url += '?' + $.param({
             'address': address,
             'section': self.section()
         });
+
+        // Clear the list in sidebar and show loading image
         self.clearVenues();
         self.isWaiting(true);
-        self.isInfo(false);
+        self.showInfo(false);
+
+        // Fetch venues data from the server
         $.ajax({
             url: url,
             method: 'GET',
         }).done(function(response) {
-            // console.log(response.meta);
+
+            // When server returns 'OK' response, set the map center according to the response
             if (response.meta.status === 'OK') {
-                // console.log(response.results.location);
                 googleMap.center = response.results.location;
                 googleMap.map.setCenter(response.results.location);
                 googleMap.bounds = new google.maps.LatLngBounds();
                 const venueItems = response.results.items;
-                // console.log(venueItems);
+                // If server returns no venueItems, notify user
                 if (venueItems.length === 0) {
                     self.displayInfo('No result', "Couldn't find " + self.section() + " in this location.");
                 } else {
+                    // If server returns venueItems create markers and list items
                     for (const venueItem of venueItems) {
                         let venue = new Venue(venueItem);
                         let marker = googleMap.createMarker(venue);
@@ -165,28 +209,46 @@ let ViewModel = function(data) {
                         self.venues.push(venue);
                     }
                     googleMap.map.fitBounds(googleMap.bounds);
+                    // Remove loading image from sidebar
                     self.isWaiting(false);
                 }
+                // Store the query data in localStorage
                 let data = {city: self.city(), country: self.country(), location: response.results.location, section: self.section()};
                 localStorage.setItem('neighborhood_map_data', JSON.stringify(data));
+
             } else if (response.meta.status === 'WRONG_ADDRESS') {
+                // If server returns 'Location Error' response, notify user
                 self.displayInfo('Location Error', "Couldn't find the location. Please specify City and Country.");
+
             } else {
+                // If server returns other error response, notify user
                 self.displayInfo('Server Error', "Couldn't receive data from the server.");
             }
+
         }).fail(function(err) {
-            // console.log(err);
+            // If AJAX request fails, notify user
             self.displayInfo('Server Error', "Couldn't receive data from the server.");
         });
     };
 
+
+    /**
+    * When user clicked list item or marker, choose the venue accordingly
+    * and show infoWindow for the venue
+    * @param {Object} venue A Venue object
+    */
     self.chooseVenue = function(venue) {
         self.chosenVenue(venue);
+        // Remove focus on other markers
         viewModel.unfocusMarkers();
         venue.marker.setIcon('/static/img/icon_focus.png');
         googleMap.populateInfoWindow(venue.marker, venue, googleMap.infoWindow);
     };
 
+
+    /**
+    * Remove all venues from ViewModel and delete all markers
+    */
     self.clearVenues = function() {
         self.venues().forEach(function(venue) {
             venue.marker.setMap(null);
@@ -195,6 +257,10 @@ let ViewModel = function(data) {
         self.venues([]);
     };
 
+
+    /**
+    * Toggle filter mode and change filter button color/icon
+    */
     self.toggleFilter = function() {
         self.isFiltered(!self.isFiltered());
         if (self.isFiltered()) {
@@ -207,6 +273,11 @@ let ViewModel = function(data) {
         }
     };
 
+
+    /**
+    * When filter mode is on, show only filtered venues and markers
+    * When off, show all venues and markers
+    */
     self.filteredVenues = ko.computed(function() {
         switch (self.isFiltered()) {
         case true:
@@ -225,49 +296,70 @@ let ViewModel = function(data) {
             });
             return self.venues();
         }
-
     });
 
+
+    /**
+    * Set all markers icon as default icon
+    */
     self.unfocusMarkers = function() {
         self.venues().forEach(function(venue){
             venue.marker.setIcon('/static/img/icon_default.png');
         });
     };
 
+
+    /**
+    * Focus the marker for a venue
+    * @param {Object} venue A Venue object
+    */
     self.focusMarker = function(venue) {
         venue.marker.setIcon('/static/img/icon_focus.png');
     };
 
+
+    /**
+    * Remove focus of marker for a venue when 'mouseout' event occured
+    * @param {Object} venue A Venue object
+    */
     self.unfocusMarker = function(venue) {
+        // Check if the venue isn't chosen
         if (venue != self.chosenVenue()) {
             venue.marker.setIcon('/static/img/icon_default.png');
         }
     };
 
+
+    /**
+    * Display information on the sidebar when error occured
+    * @param {string} title
+    * @param {string} message
+    */
     self.displayInfo = function(title, message) {
         self.infoTitle(title);
         self.infoMessage(message);
         self.isWaiting(false);
-        self.isInfo(true);
+        self.showInfo(true);
     };
-
 
 };
 
 
 
-
+// Initialize ViewModel with data from localStorage or default settings
 let data = (JSON.parse(localStorage.getItem('neighborhood_map_data')) || DEFAULT_DATA);
 let viewModel = new ViewModel(data);
 ko.applyBindings(viewModel);
+// After initializing the ViewModel, fetch venues data from server
 viewModel.searchVenues();
 
 
-//
-// Initialize Google Maps API
-//
+/**
+* Google Map API callback function
+* - Initialize map when the page is loaded
+*/
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
+    // Creates a new map object and store it in GoogleMap object.
     googleMap.map = new google.maps.Map(document.getElementById('google-map'), {
         center: data.location,
         zoom: 14,
@@ -276,23 +368,28 @@ function initMap() {
     });
 
     googleMap.infoWindow = new google.maps.InfoWindow({maxWidth: 300});
-    google.maps.event.addListener(googleMap.infoWindow, 'domready', function() {
 
-      // Reference to the DIV that wraps the bottom of infowindow
+    // Customize infoWindow styles
+    google.maps.event.addListener(googleMap.infoWindow, 'domready', function() {
+      // Remove outer box of the infoWindow
       let iwOuter = $('.gm-style-iw');
       let iwBackground = iwOuter.prev();
       iwBackground.children(':nth-child(2)').css({'display' : 'none'});
       iwBackground.children(':nth-child(4)').css({'display' : 'none'});
 
-      // Reference to the div that groups the close button elements.
+      // Styles for the close button of the infoWindow
       let iwCloseBtn = iwOuter.next();
       iwCloseBtn.css({opacity: '1', border: '7px solid #494f53', 'border-radius': '13px', 'box-shadow': '0 0 5px #373a3c'});
     });
 
+    // Event lister for display map and layout
     google.maps.event.addDomListenerOnce(googleMap.map, 'idle', function () {
+        // When window is resized, redraw the map
 	    google.maps.event.addDomListener(window, 'resize', function () {
 	        googleMap.map.fitBounds(googleMap.bounds);
 	    });
+
+        // If window.innerWidth is larger than 991px, show sidebar
         if (window.innerWidth > 991) {
             toggleSidebar();
         }
--- neighborhood_map/static/js/ui.js
+++ neighborhood_map/static/js/ui.js
@@ -1,3 +1,6 @@
+/**
+* Toggle sidebar show mode on/off
+*/
 function toggleSidebar() {
     $('#sidebar-toggle-btn').find('i').toggleClass('fa-arrow-right').toggleClass('fa-arrow-left');
     $('#content-wrapper').toggleClass('toggled');
@@ -6,5 +9,6 @@ function toggleSidebar() {
 
 
 $('document').ready(function(){
+    // Add event listener for the toggle button
     $('#sidebar-toggle-btn').click(toggleSidebar);
 });
